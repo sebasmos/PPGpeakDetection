071956411ccf60488e819267bbf9cf4bbe48414f
Sebastian Cajas Ordoñez <sebasticajas@gmail.com>
2019-01-22 15:03:27 -0500

    Señal ppg adaptada

--- PeaksDetection1.m
+++ PeaksDetection1.m
@@ -4,10 +4,10 @@
 % which are recorded from the wrist of each subject. The last three rows are 
 % simultaneous recordings of acceleration data (in x-, y-, and z-axis)
 %% ACTIVIDADES TIPO 1
-    figure(1)
+    figure(1) 
     ppg=load('DATA_01_TYPE01.mat');
     ppgSignal = ppg.sig;
-    pfinal = ppgSignal(3,(1:2000));
+    pfinal = ppgSignal(3,(1:3050));
 %FRECUENCIA DE MUESTREO
     Fs = 125;
 % CONVERSIÓN A VARIABLES FÍSICAS
@@ -17,30 +17,57 @@
 % NORMALIZACIÓN POR MÁXIMOS Y MÍNIMOS
     s2Norm = (s2-min(s2))/(max(s2)-min(s2));
 
-    t = (0:length(ppgSignal)-1)/Fs;
+    t = (0:length(pfinal)-1);
 
-    plot(s2Norm), grid on
+    plot(t,s2Norm), grid on
      
-% ENCONTRAR PICOS
-  [PKS,LOCS]=findpeaks(s2Norm,Fs,'MinPeakWidth',0.11,'MaxPeakWidth',0.5, ...
+%% ENCONTRAR PICOS
+% Se detectan picos con un ancho de mínimo 0.11 y máximo 0.5
+% y con una altura mínimo de 0.15
+  [PKS,LOCS]=findpeaks(s2Norm,'MinPeakWidth',0.11,'MaxPeakWidth',0.5, ...
               'Annotate','extents','MinPeakProminence',0.15)
           
-    axis([0 20 0.2 1 ])
-
+ %axis([0 20 0.2 1 ])
+%% Graficar los puntos picos usando los valores almacenados en PKS, LOCS
  figure(2)
  findpeaks(s2Norm,Fs,'MinPeakWidth',0.11,'MaxPeakWidth',0.5, ...
               'Annotate','extents','MinPeakProminence',0.15)
-%  plot(s2Norm),grid on
+% plot(s2Norm),grid on
   hold on
   plot(LOCS,PKS,'o')
-% Example 3:
-    %   Plot all peaks of a chirp signal whose widths are between .5 and 1 
-    %   milliseconds.
-%     Fs = 44.1e3; N = 1000;
-%     x = sin(2*pi*(1:N)/N + (10*(1:N)/N).^2);
-%     findpeaks(x,Fs,'MinPeakWidth',.5e-3,'MaxPeakWidth',1e-3, ...
-%               'Annotate','extents')
-%     p
+   
+%% Modelo pulso PPG
+
+%% Función Gaussiana
+Sigma = 0.4;
+mu = 2.5;
+AmplitudGaussiana = 0.12;
+a = AmplitudGaussiana/sqrt(2*pi*Sigma.^2);
+x = 0:1:20
+g = ((x-mu)/Sigma).^2;
+f = a*exp(-0.5*g);
+ 
+y = gaussmf(x,[2 5]);
+
+%% Función LogNormal
+% Y = lognpdf(X,mu,sigma) returns values at X of the 
+% lognormal pdf with distribution parameters mu and 
+% sigma. mu and sigma are the mean and standard deviation,
+% respectively, of the associated normal distribution
+sigma2 = 1;
+mu2 = 0.7;
+x2 = 0:1:20;
+% a1 = x1.*sqrt(2*pi*c1.^2);
+% g1 = log(((x1-b1)/2*c1).^2);2% f1 = a1*exp(-0.5*g1);
+% plot(f1)
+% Amplitud LogNormal
+ AmplLogNormal = 1.23;
+ y2 = AmplLogNormal.*lognpdf(x2,mu2,sigma2);
+ 
+ hold on
+ PPGSignal1 = y2+f;
+ plot(PPGSignal1), grid on
+  
  %% HeartRate Detection alg using ECG Signals
 % La señal cuenta con 3.7 e 4 
 % uncomment for octave under windows
--- ppgSignalModel.m
+++ ppgSignalModel.m
@@ -1,30 +1,37 @@
 % % Modelo pulso PPG
 
-c = 0.4;
-b = 2.5;
-a = 0.05/sqrt(2*pi*c.^2);
+%% Función Gaussiana
+Sigma = 0.4;
+mu = 2.5;
+AmplitudGaussiana = 0.12;
+a = AmplitudGaussiana/sqrt(2*pi*Sigma.^2);
 x = 0:0.1:10
-g = ((x-b)/c).^2;
+g = ((x-mu)/Sigma).^2;
 f = a*exp(-0.5*g);
  
 x = 0:0.1:10;
 y = gaussmf(x,[2 5]);
 
-c1 = 1;
-b1 = 0.7;
-x1 = 0:0.1:10;
+%% Función LogNormal
+% Y = lognpdf(X,mu,sigma) returns values at X of the 
+% lognormal pdf with distribution parameters mu and 
+% sigma. mu and sigma are the mean and standard deviation,
+% respectively, of the associated normal distribution
+sigma2 = 1;
+mu2 = 0.7;
+x2 = 0:0.1:10;
 % a1 = x1.*sqrt(2*pi*c1.^2);
-% g1 = log(((x1-b1)/2*c1).^2);
-% f1 = a1*exp(-0.5*g1);
+% g1 = log(((x1-b1)/2*c1).^2);2% f1 = a1*exp(-0.5*g1);
 % plot(f1)
- y2 = 1.*lognpdf(x1,b1,c1);
+% Amplitud LogNormal
+ AmplLogNormal = 1.23;
+ y2 = AmplLogNormal.*lognpdf(x2,mu2,sigma2);
  
  figure(1)
- h1 = y2+f;
- plot(h1)
+ PPGSignal1 = y2+f;
+ plot(PPGSignal1), grid on
  %plot(x1,y2)
  
 
-%  hold on
 %  h2 = f + y;
  
