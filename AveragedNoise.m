2f70eec20286c2a43eee84c244986eb660147eee
Sebastian Cajas Ordoñez <sebasticajas@gmail.com>
2019-02-06 10:34:50 -0500

    Ruidos por sebastian

--- AveragedNoise.m
+++ AveragedNoise.m
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 clear all
 clc
 %% RUIDO EN REPOSO PRIMEROS 30 SEGUNDOS
@@ -9,29 +9,36 @@ sm2=0;
 sm3=0;
 sm4=0;
 sm5=0;
+
 for k = 1:12
     if k >= 10
         labelstring = int2str(k);
         word = strcat({'DATA_'},labelstring,{'_TYPE02.mat'});
         a = load(char(word));
-        TamRealizaciones(k,:) = length(a.sig);
-        s(k,:) =  GetSavitzkyNoise(char(word),2,1,3750);
-        s1(k,:) =  GetSavitzkyNoise(char(word),2,3751,11250);
-        s2(k,:) =  GetSavitzkyNoise(char(word),2,11250,18750);
-        s3(k,:) =  GetSavitzkyNoise(char(word),2,18750,26250);
-        s4(k,:) =  GetSavitzkyNoise(char(word),2,26250,33750);        
-        s5(k,:) =  GetSavitzkyNoise(char(word),2,33750,35989);
+        TamRealizaciones(k,:) = length(a.sig)
     else
         labelstring = int2str(k);
         word = strcat({'DATA_0'},labelstring,{'_TYPE02.mat'});
         a = load(char(word));
         TamRealizaciones(k,:) = length(a.sig);
+    end
+end
+
+for k = 1:12
+    if k >= 10
         s(k,:) =  GetSavitzkyNoise(char(word),2,1,3750);
         s1(k,:) =  GetSavitzkyNoise(char(word),2,3751,11250);
         s2(k,:) =  GetSavitzkyNoise(char(word),2,11250,18750);
         s3(k,:) =  GetSavitzkyNoise(char(word),2,18750,26250);
         s4(k,:) =  GetSavitzkyNoise(char(word),2,26250,33750);        
-        s5(k,:) =  GetSavitzkyNoise(char(word),2,33750,35989);
+        s5(k,:) =  GetSavitzkyNoise(char(word),2,33750,min(TamRealizaciones));
+    else       
+        s(k,:) =  GetSavitzkyNoise(char(word),2,1,3750);
+        s1(k,:) =  GetSavitzkyNoise(char(word),2,3751,11250);
+        s2(k,:) =  GetSavitzkyNoise(char(word),2,11250,18750);
+        s3(k,:) =  GetSavitzkyNoise(char(word),2,18750,26250);
+        s4(k,:) =  GetSavitzkyNoise(char(word),2,26250,33750);        
+        s5(k,:) =  GetSavitzkyNoise(char(word),2,33750,min(TamRealizaciones));
     end
     sm0 = sm0 + s(k,:);
     sm1 = sm1 + s(k,:);
@@ -43,26 +50,4 @@ end
 %% PROMEDIO DE LOS RUIDOS
 total1 = sm0./12;
 plot(total1);
-=======
 
-clear all
-clc
-%% RUIDO EN REPOSO PRIMEROS 30 SEGUNDOS
-k=0;
-suma = 0;
-for k = 1:12
-    if k >= 10
-        labelstring = int2str(k);
-        word = strcat({'DATA_'},labelstring,{'_TYPE02.mat'});
-        s(k,:) =  GetSavitzkyNoise(char(word),3,1,3750);
-    else
-    labelstring = int2str(k);
-    word = strcat({'DATA_0'},labelstring,{'_TYPE02.mat'});
-    s(k,:) =  GetSavitzkyNoise(char(word),3,1,3750);
-    end
-    suma = suma + s(k,:);
-end
-%% PROMEDIO DE LOS RUIDOS
-total1 = suma./12;
-plot(total1);
->>>>>>> 4751b791c4c11d23a5c5d2ea3a127d92c97dd5cf
