a39c09a74146dfb753f3d84a82e83ce6fe617fef
malelang <landiezm@unicauca.edu.co>
2019-02-07 10:02:21 -0500

    ruido generado

--- AveragedNoise.m
+++ AveragedNoise.m
@@ -73,4 +73,20 @@ varianzamuestral= var(V);
 
 
 %% pdf 
-[MX,MY]=Distribucion(mediamuestral,varianzamuestral);
+[MX,MY]=Distribucion(mediamuestral,varianzamuestral); %sacamos 
+for i=1:length(MY)
+    maximo=max(MY(i,:));
+    minimo=min(MY(i,:));
+    nuevoMY(i,:)=(MY(i,:)-minimo)./(maximo-minimo);
+end
+W=zeros(5,length(mediamuestral));
+for k=1:length(mediamuestral)
+    W(:,k)=mediamuestral(k)+sqrt(varianzamuestral(k))*randn(5,1);
+end
+%%
+t = (0:length(W)-1);
+figure(2)
+plot(t,W(1,:)./10),hold on,grid on, 
+
+%%
+
