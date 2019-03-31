4dd6e1d243e795ddaeb4aadd4afa9688b24ce037
Sebastian Cajas Ordoñez <sebasticajas@gmail.com>
2019-03-31 14:19:38 -0500

    parametrizado

--- NoiseProofs/DenoiseECG.m
+++ NoiseProofs/DenoiseECG.m
@@ -1,3 +1,8 @@
+%% function [denoised]=DenoiseECG(ECGsignal)
+% This function allows determining ECGsignal without low frequency noise
+% components
+% ECGSignal: input signal
+% 10 R
 
 function [denoised]=DenoiseECG(ECGsignal)
     p = Detrending(ECGsignal,10);
