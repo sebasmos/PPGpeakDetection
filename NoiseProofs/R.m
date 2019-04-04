503dcf9ce4e1b3afc56152bfc7e7cf35d5b612e8
malelang <landiezm@unicauca.edu.co>
2019-04-04 07:03:18 -0500

    before pull

---
+++ NoiseProofs/R.m
@@ -0,0 +1,59 @@
+addpath('/Users/alejandralandinez/Documents/MATLAB/mcode/tesis/Training_data/db');
+
+
+% Lecture of datasets
+for k = 1:12
+    if k >= 10
+        labelstring = int2str(k);
+        word = strcat({'DATA_'},labelstring,{'_TYPE02.mat'});
+        a = load(char(word));
+        TamRealizaciones(k) = length(a.sig);
+    else
+        labelstring = int2str(k);
+        word = strcat({'DATA_0'},labelstring,{'_TYPE02.mat'});
+        a = load(char(word));
+        TamRealizaciones(k) = length(a.sig);
+    end
+end
+%%
+
+for k = 1:12
+    if k >= 10
+        labelstring = int2str(k);
+        word = strcat({'DATA_'},labelstring,{'_TYPE02.mat'});
+        s(k,:) =  GetSavitzkyNoise(char(word),2,1,3750);
+        s1(k,:) =  GetSavitzkyNoise(char(word),2,3751,11250);
+        s2(k,:) =  GetSavitzkyNoise(char(word),2,11251,18750);
+        s3(k,:) =  GetSavitzkyNoise(char(word),2,18751,26250);
+        s4(k,:) =  GetSavitzkyNoise(char(word),2,26251,33750);        
+        s5(k,:) =  GetSavitzkyNoise(char(word),2,33751,min(TamRealizaciones));
+    else       
+        labelstring = int2str(k);
+        word = strcat({'DATA_0'},labelstring,{'_TYPE02.mat'});
+        s(k,:) =  GetSavitzkyNoise(char(word),2,1,3750);
+        s1(k,:) =  GetSavitzkyNoise(char(word),2,3751,11250);
+        s2(k,:) =  GetSavitzkyNoise(char(word),2,11251,18750);
+        s3(k,:) =  GetSavitzkyNoise(char(word),2,18751,26250);
+        s4(k,:) =  GetSavitzkyNoise(char(word),2,26251,33750);        
+        s5(k,:) =  GetSavitzkyNoise(char(word),2,33751,min(TamRealizaciones));
+    end
+end
+%%
+V=[s s1 s2 s3 s4 s5];
+
+%%
+i=1;
+j=1;
+for t1=1:10:length(ruido)
+    for t2=1:10:length(ruido)
+        for k=1:12
+            values(k)=V(k,t1).*V(k,t2);
+        end
+        Rxx(i,j)=mean(values);
+        Kxx(i,j)=Rxx(i,j)-(h(t1).*h(t2));
+        j=j+1;
+        values=[];
+    end
+    i=i+1;
+    j=1;
+end
