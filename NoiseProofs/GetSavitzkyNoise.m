ef9e53abd2eea151e9569dab46e0725ee5d2d400
Sebastian Cajas Ordoñez <sebasticajas@gmail.com>
2019-04-11 14:29:08 -0500

    arreglado

--- NoiseProofs/GetSavitzkyNoise.m
+++ NoiseProofs/GetSavitzkyNoise.m
@@ -25,6 +25,6 @@ addpath('/Users/alejandralandinez/Documents/MATLAB/mcode/tesis/Training_data/db'
 %         media = Detrending(sNorm,10);
 %     end
     media=ValoresMedia(sNorm);
-    Noise=sNorm-sfilt;
+    Noise=sNorm-sfilt+media;
 
 end
